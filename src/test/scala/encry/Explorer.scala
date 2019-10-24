ad8b88a341b6636a879ed65b5310e7750e0cf5bc
SunFrost <moskalenkosv@gmail.com>
2019-10-24 13:04:57 +0500

    add requests

--- src/test/scala/encry/Explorer.scala
+++ src/test/scala/encry/Explorer.scala
@@ -16,9 +16,52 @@ object Explorer {
     )
   }
 
-  val scenario: ChainBuilder =
+  def getBlocks: ChainBuilder = {
     exec(
-      getIndex
+      http("blocks")
+        .get("/")
+        .check(
+          status is 200
+        )
     )
+  }
+
+  def getBlock(id: String): ChainBuilder = {
+    exec(
+      http("block")
+        .get(s"/block/$id")
+        .check(
+          status is 200
+        )
+    )
+  }
+
+  def getTrans(id: String): ChainBuilder = {
+    exec(
+      http("trans")
+        .get(s"/transaction/$id")
+        .check(
+          status is 200
+        )
+    )
+  }
+
+  def getNodes: ChainBuilder = {
+    exec(
+      http("nodes")
+        .get("/node")
+        .check(
+          status is 200
+        )
+    )
+  }
+
+  val scenario =
+    exec(
+      getBlock("0052d16317160f3579563691033c916a0be2b405a791dacf2a685cde2aa68a00")
+    ).pause(1)
+      .exec(
+        getTrans("ba0de6b393e736dc23551a7daf3b05b3e2ff719d58bafcda627a1594a0dc6c44")
+      )
 
 }
